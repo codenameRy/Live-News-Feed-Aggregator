2b773ed0c903edfafe85f3a73fe138641ff3afd1
Ryan Lee <ryanm.lee11@gmail.com>
2020-11-08 22:03:37 -0500

    API Fetch function to retrieve news listing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Pusher from 'pusher-js';
+import pushid from 'pushid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    newsItems: [],
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/live')
+    .then(response => response.json())
+    .then(articles => {
+      this.setState({
+        newsItems: [...this.state.news, ...articles],
+      });
+    }).catch(error => console.log(error));
+
+    const pusher = new Pusher
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
