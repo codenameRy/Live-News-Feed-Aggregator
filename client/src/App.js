09728fd60605c8dff1ca26118643e234b1e3291e
Ryan Lee <ryanm.lee11@gmail.com>
2020-11-08 23:04:35 -0500

    Styling updates

--- client/src/App.js
+++ client/src/App.js
@@ -17,13 +17,32 @@ class App extends Component {
       });
     }).catch(error => console.log(error));
 
-    const pusher = new Pusher
+    const pusher = new Pusher('597ccdfc5391601fe1f8', {
+      cluster: 'mt1',
+      encrypted: true,
+    });
+
+    const channel = pusher.subscribe('news-channel');
+    channel.bind('update-news', data => {
+      this.setState({
+        newsItems: [...data.articles, ...this.state.newsItems],
+      });
+    });
+
   }
 
   render() {
+    const NewsItem = (article, id) => (
+      <li key={id}><a href={`${article.url}`}>{article.title}</a></li>
+    );
+
+    const newsItems = this.state.newsItems.map(e => NewsItem(e, pushid()));
+
     return (
-      <div>
-        
+      <div className="App">
+        <h1 className="App-title">Live News Feed</h1>
+
+        <ul className="news-items">{newsItems}</ul>
       </div>
     );
   }
