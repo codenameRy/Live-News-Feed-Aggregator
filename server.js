f3f3ddcb4119acee0110cd4756bfb7303ca3f3b5
Ryan Lee <ryanm.lee11@gmail.com>
2020-11-09 22:23:10 -0500

    Updates

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-require('dotenv').config({path: 'variables.env'})
+require('dotenv').config({path: 'variables.env'});
+
+const express = require('express');
+const cors = require('cors');
+const Pusher = require('pusher');
+const NewsAPI = require('newsapi');
+
+const app = express();
+
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_AP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: process.env.PUSHER_APP_CLUSTER,
+    encrypted: true,
+});
+
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+
+const fetchNews = (searchTerm, pageNum) => 
+newsapi.v2.everything({
+    q: searchTeam,
+    language: 'en',
+    page: pageNum,
+    pageSize: 5,
+});
